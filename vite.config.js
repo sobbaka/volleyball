b825699d9c3da0cc19ce056b1c9bdc122fc42e21
sobbaka <a.bagaev1989@gmail.com>
2024-01-18 18:03:07 +0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+const outDir = resolve(__dirname, 'docs')
+const root = resolve(__dirname, 'src')
+const publicFolder = resolve(__dirname, 'public')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: root,
+    base: './',
+    publicDir: publicFolder,
+    server: {
+        watch: {
+            usePolling: true
+        },
+        open: true
+    },
+    build: {
+        outDir: outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                english: resolve(__dirname, 'src/english.html'),
+                georgian: resolve(__dirname, 'src/georgian.html'),
+            },
+        }
+    },
+})
