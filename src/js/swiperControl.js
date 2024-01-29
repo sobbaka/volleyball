ee458e31bdf837d13316fd3e8a103df9d92e5845
sobbaka <a.bagaev1989@gmail.com>
2024-01-29 18:33:52 +0400

    add contact block

--- src/js/swiperControl.js
+++ src/js/swiperControl.js
@@ -28,10 +28,10 @@ const serviceSwiper = new Swiper(".swiper-service", {
   slidesPerView: 1.62,
   spaceBetween: 20,
   // loop: true,
-  navigation: {
-    nextEl: ".camps-swiper__button-next",
-    prevEl: ".camps-swiper__button-prev",
-  },
+  // navigation: {
+  //   nextEl: ".camps-swiper__button-next",
+  //   prevEl: ".camps-swiper__button-prev",
+  // },
 });
 
 
