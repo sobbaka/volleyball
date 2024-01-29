8666598fae00979cc90a9bff0afd399ab5eb1942
sobbaka <a.bagaev1989@gmail.com>
2024-01-29 21:59:58 +0400

    change hero desktop styles

--- src/js/main.js
+++ src/js/main.js
@@ -9,35 +9,35 @@ import './swiperControl.js';
 
 
 
-const leftElements = document.querySelectorAll('.hero__scale-left')
-const rightElements = document.querySelectorAll('.hero__scale-right')
+// const leftElements = document.querySelectorAll('.hero__scale-left')
+// const rightElements = document.querySelectorAll('.hero__scale-right')
 
 
-const heroContent = document.querySelector('.hero__content');
-const styles = window.getComputedStyle(heroContent);
-const paddingBottom = parseFloat(styles.getPropertyValue('padding-bottom'));
-const heroContentHeight = heroContent.getBoundingClientRect().height;
+// const heroContent = document.querySelector('.hero__content');
+// const styles = window.getComputedStyle(heroContent);
+// const paddingBottom = parseFloat(styles.getPropertyValue('padding-bottom'));
+// const heroContentHeight = heroContent.getBoundingClientRect().height;
 
-const applyScale = (elements, scale, origin) => {
-  elements.forEach(element => {
-    element.style.transform = `scale(${scale})`;
-    element.style.transformOrigin = origin;
-  });
-};
+// const applyScale = (elements, scale, origin) => {
+//   elements.forEach(element => {
+//     element.style.transform = `scale(${scale})`;
+//     element.style.transformOrigin = origin;
+//   });
+// };
 
-const handleResize = () => {
-  const screenWidth = window.innerWidth;
-  const scale = screenWidth <= 1320 ? screenWidth / 1320 : 1;
-  const padding = screenWidth <= 1320 ? scale * 0.5 : 1;
-  applyScale(leftElements, scale, 'left top');
-  applyScale(rightElements, scale, 'right top');
-  heroContent.style.paddingBottom = `${paddingBottom * padding}px`;
-  heroContent.style.height = `${heroContentHeight * scale}px`;
-};
+// const handleResize = () => {
+//   const screenWidth = window.innerWidth;
+//   const scale = screenWidth <= 1320 ? screenWidth / 1320 : 1;
+//   const padding = screenWidth <= 1320 ? scale * 0.5 : 1;
+//   applyScale(leftElements, scale, 'left top');
+//   applyScale(rightElements, scale, 'right top');
+//   heroContent.style.paddingBottom = `${paddingBottom * padding}px`;
+//   heroContent.style.height = `${heroContentHeight * scale}px`;
+// };
 
-window.addEventListener('resize', handleResize);
-// Initial call to apply the scaling when the script loads
-handleResize();
+// window.addEventListener('resize', handleResize);
+// // Initial call to apply the scaling when the script loads
+// handleResize();
 
 
 
